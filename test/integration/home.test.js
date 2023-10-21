6a6ae6fcbbb281c7be99b31f00e1a25b38ff259a
Jake Champion <jchampion@fastly.com>
2023-10-21 01:31:56 +0100

    ok

--- test/integration/home.test.js
+++ test/integration/home.test.js
@@ -6,7 +6,7 @@ import assert from "node:assert";
 import axios from "./helpers.js";
 
 describe("GET /", function() {
-	it("responds with a 301 status", async () => {
+	it("responds with a 200 status", async () => {
 		const response = await axios.get(`/`);
 		assert.equal(response.status, 200);
 	});
